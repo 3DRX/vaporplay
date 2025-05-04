37b6593166bf58a0a92f10c1b729a76d05bd6746
3DRX <3drxkjy@gmail.com>
2025-05-04 16:19:03 +0800

    combobox

--- client/vaporplay-native-client/ui/ui.go
+++ client/vaporplay-native-client/ui/ui.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"fmt"
 	"image"
+	"image/color"
 	"log/slog"
 	"os"
 	"sync"
@@ -31,6 +32,11 @@ type ebitenGame struct {
 	lock sync.Mutex
 }
 
+type ListEntry struct {
+	id   int
+	name string
+}
+
 func NewUIThread(
 	frameChan <-chan image.Image,
 	configPath *string,
@@ -52,8 +58,17 @@ func NewUIThread(
 			),
 		),
 	)
+	serverLabel := widget.NewText(
+		widget.TextOpts.Text("Server URL", face, hexToColor(textIdleColor)),
+		widget.TextOpts.Position(widget.TextPositionCenter, widget.TextPositionCenter),
+		widget.TextOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionStart,
+			}),
+		),
+	)
+	root.AddChild(serverLabel)
 	serverInput := widget.NewTextInput(
-		widget.TextInputOpts.Placeholder("Server URL"),
 		widget.TextInputOpts.WidgetOpts(
 			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
 				Position: widget.RowLayoutPositionCenter,
@@ -80,6 +95,112 @@ func NewUIThread(
 		}),
 	)
 	root.AddChild(serverInput)
+	btnImg := &widget.ButtonImage{
+		Idle:    eimage.NewNineSliceColor(color.NRGBA{R: 170, G: 170, B: 180, A: 255}),
+		Hover:   eimage.NewNineSliceColor(color.NRGBA{R: 130, G: 130, B: 150, A: 255}),
+		Pressed: eimage.NewNineSliceColor(color.NRGBA{R: 100, G: 100, B: 120, A: 255}),
+	}
+	ent := []ListEntry{
+		{
+			id:   0,
+			name: "H.264 NVENC",
+		},
+		{
+			id:   1,
+			name: "H.265 NVENC",
+		},
+		{
+			id:   2,
+			name: "AV1 NVENC",
+		},
+		{
+			id:   3,
+			name: "x264",
+		},
+	}
+	entries := make([]any, 0, len(ent))
+	for _, e := range ent {
+		entries = append(entries, e)
+	}
+	comboBox := widget.NewListComboButton(
+		widget.ListComboButtonOpts.SelectComboButtonOpts(
+			widget.SelectComboButtonOpts.ComboButtonOpts(
+				//Set the max height of the dropdown list
+				widget.ComboButtonOpts.MaxContentHeight(150),
+				//Set the parameters for the primary displayed button
+				widget.ComboButtonOpts.ButtonOpts(
+					widget.ButtonOpts.Image(btnImg),
+					widget.ButtonOpts.TextPadding(widget.NewInsetsSimple(5)),
+					widget.ButtonOpts.Text("", face, &widget.ButtonTextColor{
+						Idle:     hexToColor(textIdleColor),
+						Disabled: hexToColor(textDisabledColor),
+					}),
+					widget.ButtonOpts.WidgetOpts(
+						//Set how wide the button should be
+						widget.WidgetOpts.MinSize(150, 0),
+						//Set the combobox's position
+						widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+							HorizontalPosition: widget.AnchorLayoutPositionCenter,
+							VerticalPosition:   widget.AnchorLayoutPositionCenter,
+						})),
+				),
+			),
+		),
+		widget.ListComboButtonOpts.ListOpts(
+			//Set how wide the dropdown list should be
+			widget.ListOpts.ContainerOpts(widget.ContainerOpts.WidgetOpts(widget.WidgetOpts.MinSize(150, 0))),
+			//Set the entries in the list
+			widget.ListOpts.Entries(entries),
+			widget.ListOpts.ScrollContainerOpts(
+				//Set the background images/color for the dropdown list
+				widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
+					Idle:     eimage.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+					Disabled: eimage.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+					Mask:     eimage.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				}),
+			),
+			widget.ListOpts.SliderOpts(
+				//Set the background images/color for the background of the slider track
+				widget.SliderOpts.Images(&widget.SliderTrackImage{
+					Idle:  eimage.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+					Hover: eimage.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
+				}, btnImg),
+				widget.SliderOpts.MinHandleSize(5),
+				//Set how wide the track should be
+				widget.SliderOpts.TrackPadding(widget.NewInsetsSimple(2))),
+			//Set the font for the list options
+			widget.ListOpts.EntryFontFace(face),
+			//Set the colors for the list
+			widget.ListOpts.EntryColor(&widget.ListEntryColor{
+				Selected:                   color.NRGBA{254, 255, 255, 255},             //Foreground color for the unfocused selected entry
+				Unselected:                 color.NRGBA{254, 255, 255, 255},             //Foreground color for the unfocused unselected entry
+				SelectedBackground:         color.NRGBA{R: 130, G: 130, B: 200, A: 255}, //Background color for the unfocused selected entry
+				SelectedFocusedBackground:  color.NRGBA{R: 130, G: 130, B: 170, A: 255}, //Background color for the focused selected entry
+				FocusedBackground:          color.NRGBA{R: 170, G: 170, B: 180, A: 255}, //Background color for the focused unselected entry
+				DisabledUnselected:         color.NRGBA{100, 100, 100, 255},             //Foreground color for the disabled unselected entry
+				DisabledSelected:           color.NRGBA{100, 100, 100, 255},             //Foreground color for the disabled selected entry
+				DisabledSelectedBackground: color.NRGBA{100, 100, 100, 255},             //Background color for the disabled selected entry
+			}),
+			//Padding for each entry
+			widget.ListOpts.EntryTextPadding(widget.NewInsetsSimple(5)),
+		),
+		//Define how the entry is displayed
+		widget.ListComboButtonOpts.EntryLabelFunc(
+			func(e any) string {
+				//Button Label function
+				return "Button: " + e.(ListEntry).name
+			},
+			func(e any) string {
+				//List Label function
+				return "List: " + e.(ListEntry).name
+			}),
+		//Callback when a new entry is selected
+		widget.ListComboButtonOpts.EntrySelectedHandler(func(args *widget.ListComboButtonEntrySelectedEventArgs) {
+			fmt.Println("Selected Entry: ", args.Entry)
+		}),
+	)
+	comboBox.SetSelectedEntry(entries[0])
+	root.AddChild(comboBox)
 
 	game := &ebitenGame{
 		closeWindowPromise: closeWindowPromise,
